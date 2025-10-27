84b1eb6988f28a21c464ec9d2787f091139fab4f
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-26 21:03:02 -0600

    Update index.js

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -1,16 +1,17 @@
 // Importamos la librería Express
 const express = require('express');
+require('dotenv').config(); // Importamos dotenv para leer variables de entorno
 
 // Creamos una instancia de la aplicación
 const app = express();
 
 // Definimos el puerto en el que escuchará el servidor.
-// Tomará el valor de la variable de entorno PORT que definimos en docker-compose.yml, o 3000 si no existe.
+// Tomará el valor de la variable de entorno PORT, o 3000 si no existe.
 const PORT = process.env.PORT || 3000;
 
 // Definimos una ruta de prueba en la raíz del servidor
-app.get('/', (req, res) => {
-  res.json({ message: '¡El microservicio de autenticación está vivo!' });
+app.get('/api/auth', (req, res) => {
+  res.status(200).json({ message: '¡El microservicio de autenticación está vivo y responde!' });
 });
 
 // Iniciamos el servidor para que escuche peticiones en el puerto especificado
