5b65e6e95c66bd331d3c5941bea2204c3969fbf5
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-28 00:21:06 -0600

    Create RegisterPage.jsx

---
+++ frontend-service/src/pages/RegisterPage.jsx
@@ -0,0 +1,14 @@
+// src/pages/RegisterPage.jsx
+import React from 'react';
+
+function DashboardPage() {
+    return (
+        <div>
+            <h2>Registrar nuevo usuario</h2>
+            <p>¡Bienvenido al CRM de Consilium!</p>
+            {/* Más adelante añadiremos el botón de logout aquí */}
+        </div>
+    );
+}
+
+export default DashboardPage;
