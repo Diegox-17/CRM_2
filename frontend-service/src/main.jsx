f58242d35efb10586d8a010ad6ade6c8985abeb7
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-28 00:47:06 -0600

    Update main.jsx

--- frontend-service/src/main.jsx
+++ frontend-service/src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import './index.css'; // Es buena idea importar un css base si lo tienes
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-)
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
+);
