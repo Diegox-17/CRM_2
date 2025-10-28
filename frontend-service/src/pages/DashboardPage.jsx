d3e75a6e2db3938a820048ed2d3de85dcfac5243
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-28 00:18:34 -0600

    Create DashboardPage.jsx

---
+++ frontend-service/src/pages/DashboardPage.jsx
@@ -0,0 +1,14 @@
+// src/pages/DashboardPage.jsx
+import React from 'react';
+
+function DashboardPage() {
+    return (
+        <div>
+            <h2>Dashboard Principal</h2>
+            <p>¡Bienvenido al CRM de Consilium!</p>
+            {/* Más adelante añadiremos el botón de logout aquí */}
+        </div>
+    );
+}
+
+export default DashboardPage;
