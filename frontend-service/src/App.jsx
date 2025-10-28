aac0816bc7e33a96d764958e00db990d307ba4df
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-28 00:25:27 -0600

    Update App.jsx

--- frontend-service/src/App.jsx
+++ frontend-service/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return (
-    <div>
-      <h1>CRM Consilium</h1>
-      <LoginPage />
-    </div>
-  );
+    return (
+        <Router>
+            <h1>CRM Consilium</h1>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/" element={<DashboardPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
