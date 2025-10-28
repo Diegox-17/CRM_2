75e0075caac30841da8ada547e2061366c748162
Diegox-17 <direccion@consiliumproyectos.com>
2025-10-28 00:34:27 -0600

    Update DashboardPage.jsx

--- frontend-service/src/pages/DashboardPage.jsx
+++ frontend-service/src/pages/DashboardPage.jsx
@@ -2,6 +2,13 @@
 import React from 'react';
 
 function DashboardPage() {
+    const { logout } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logout();
+        // No necesitamos redirigir manualmente. 
+        // El ProtectedRoute se dará cuenta de que el token ha desaparecido y nos enviará a /login.
+    };
     return (
         <div>
             <h2>Dashboard Principal</h2>
